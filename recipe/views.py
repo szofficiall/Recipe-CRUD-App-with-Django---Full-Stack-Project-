f3e77bb195f794b6fde4c9afce5575afae1b615b
szofficiall <sultaan.khan2002@gmail.com>
2026-07-16 22:47:53 +0500

    Add Advance Authentication System

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Recipe
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def splash_screen(request):
@@ -8,6 +13,75 @@ def splash_screen(request):
     return render(request, "splash.html")
 
 
+def login_page(request):
+    if (request.method == "POST"):
+        u_name = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if not User.objects.filter(username=u_name).exists():
+            messages.error(request, "Invalid User Name")
+            return redirect("login_page")
+
+        user = authenticate(request, username=u_name, password=password)
+
+        if user is None:
+            messages.error(request, "Inavlid Password")
+            return redirect("login_page")
+        else:
+            login(request, user)
+            return redirect("/recipe_view/")
+
+    return render(request, "login.html")
+
+
+def logout_page(request):
+    logout(request)
+    return redirect("/login_page/")
+
+
+def signup_page(request):
+    if (request.method == "POST"):
+        f_name = request.POST.get("first_name")
+        l_name = request.POST.get("last_name")
+        u_name = request.POST.get("username")
+        password = request.POST.get("password")
+        con_password = request.POST.get("confirm_password")
+
+        user = User.objects.filter(username=u_name)
+        # print(
+        #     f"""first name : {f_name} last name : {l_name}  user name : {u_name}  password: {password}  confirm Password : {confirm_password}  """)
+        if (user.exists()):
+            messages.error(request, "User Al ready Exist")
+            return redirect("signup_page")
+
+        if (password != con_password):
+            messages.error(request, "Password Does't Match")
+            return redirect("signup_page")
+
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            for error in e.messages:
+                messages.error(request, error)
+            return redirect("signup_page")
+
+        user = User.objects.create_user(
+            first_name=f_name,
+            last_name=l_name,
+            username=u_name,
+            password=password,
+        )
+
+        # user.set_password(password)
+        # user.save()
+
+        messages.success(request, "Account Created Succesfully")
+
+        return redirect("signup_page")
+    return render(request, "signup.html")
+
+
+@login_required(login_url="/login_page/")
 def recipe_view(request):
     if request.method == "POST":
         recipe_name = request.POST.get("recipe_name")
@@ -41,6 +115,7 @@ def recipe_view(request):
     })
 
 
+@login_required(login_url="/login_page/")
 def delete_recipe(request, id):
     recipe = get_object_or_404(Recipe, id=id)
     recipe.delete()
@@ -48,6 +123,7 @@ def delete_recipe(request, id):
     return redirect("recipe-view")
 
 
+@login_required(login_url="/login_page/")
 def update_recipe(request, id):
     recipe = get_object_or_404(Recipe, id=id)
 
--- recipeMaker_CRUD/urls.py
+++ recipeMaker_CRUD/urls.py
@@ -8,7 +8,10 @@ from recipe.views import *
 
 urlpatterns = [
     path('', splash_screen, name='splash_screen'),
+    path('login_page/', login_page, name='login_page'),
+    path('signup_page/', signup_page, name='signup_page'),
     path("recipe_view/", recipe_view, name="recipe-view"),
+    path("logout_page/", logout_page, name="logout_page"),
     path("delete_recipe/<id>/", delete_recipe, name="delete_recipe"),
     path("update_recipe/<id>/", update_recipe, name="update_recipe"),
     path('admin/', admin.site.urls),
